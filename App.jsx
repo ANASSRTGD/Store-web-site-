ddb2b101c926c2be33f0e305a19a52723779ee5f
ANASSRTGD <118916163+ANASSRTGD@users.noreply.github.com>
2026-01-05 21:39:07 +0100

    fix App.jsx structure

--- App.jsx
+++ App.jsx
@@ -1,26 +1,21 @@
-import React, { useState } from 'react'
+import { useState } from "react";
 import NavBar from "./NavBar";
-import Footer from "./Footer";
 import Home from "./Home";
 import Shop from "./Shop";
 import Contact from "./Contact";
-import About from "./About";
-export default function App() {
-  const [currentPage, setCurrentPage] = useState('home')
+import Footer from "./Footer";
+
+function App() {
+  const [currentPage, setCurrentPage] = useState("home");
 
   return (
-    <div className="app" dir="rtl">
-      <NavBar currentPage={currentPage} onNavigate={setCurrentPage} />
-      <main className="container" role="main">
-        {currentPage === 'home' && <Home onNavigate={setCurrentPage} />}
-        {currentPage === 'shop' && <Shop />}
-        {currentPage === 'about' && <About />}
-        {currentPage === 'contact' && <Contact />}
-        function App() {
-  return (
     <div>
-      <NavBar />
-      <Home />
+      <NavBar setCurrentPage={setCurrentPage} />
+
+      {currentPage === "home" && <Home />}
+      {currentPage === "shop" && <Shop />}
+      {currentPage === "contact" && <Contact />}
+
       <Footer />
     </div>
   );
