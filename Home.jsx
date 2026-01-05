9f011e5822dd34ce53758c77e8b8640525d36a2e
ANASSRTGD <118916163+ANASSRTGD@users.noreply.github.com>
2026-01-05 21:42:26 +0100

    Update import path for products in Home component

--- Home.jsx
+++ Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import products from '../data/products'
+import products from "./products";
 
 export default function Home({ onNavigate }) {
   const top = products.slice(0, 3)
